942b350bc68d7586e95c75e5d98274c8bf13ff9f
Will Glasse <wg15313@my.bristol.ac.uk>
2018-12-17 15:50:32 +0000

    clean up

--- Angular/src/app/components/image-grid/image-grid.component.ts
+++ Angular/src/app/components/image-grid/image-grid.component.ts
@@ -4,13 +4,11 @@ import { ReadImagesService } from '../../services/read-images.service';
 import { ImageData } from '../../models/image-data';
 
 @Component({
-  selector: 'app-read-images',
+  selector: 'app-image-grid',
   templateUrl: './image-grid.component.html',
   styleUrls: ['./image-grid.component.css']
 })
 export class ImageGridComponent implements OnInit {
-
-  // @Input() image: ImageData;
 
   imageList: Observable<Array<ImageData>>;
 
